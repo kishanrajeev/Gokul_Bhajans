42863cf4aa1b9640f9fd929e7593d2f4f7a5760f
Kishan <kishanrj@outlook.com>
2023-02-25 15:42:39 -0500

    Fixed the drawer.dart so it auto adjusts the About Us button to the bottom of all screens.

--- lib/view/home_screen/downlaod.dart
+++ lib/view/home_screen/downlaod.dart
@@ -1,10 +1,10 @@
+import 'dart:async';
 import 'dart:io';
-
-import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:dio/dio.dart';
+import 'package:archive/archive.dart';
 
 class Download extends StatefulWidget {
   @override
@@ -20,50 +20,49 @@ class _DownloadState extends State<Download> {
       ),
       body: Center(
         child: ElevatedButton(
-          child: Text('Download Music'),
-          onPressed: () => downloadFolder(),
+          child: Text('Download and Extract'),
+          onPressed: () => downloadAndExtract(),
         ),
       ),
     );
   }
 
-  void downloadFolder() async {
-    // Initialize dio with your OneDrive API credentials
-    var dio = Dio(BaseOptions(
-      headers: {'Authorization': 'Bearer YOUR_ACCESS_TOKEN'},
-    ));
-
-    // Set up the download directory
-    final directory = await getExternalStorageDirectory();
-    final downloadsDirectory = '${directory?.path}/Downloads';
-    final savedDir = Directory(downloadsDirectory);
-    bool hasExisted = await savedDir.exists();
-    if (!hasExisted) {
-      savedDir.create();
+  void downloadAndExtract() async {
+    // Set up the download and extraction directories
+    final downloadsDirectory = (await getExternalStorageDirectory())!.path;
+    final extractionDirectory =
+        '${(await getApplicationDocumentsDirectory()).path}/Gokulbhajans';
+    final extractionDir = Directory(extractionDirectory);
+    bool hasExtractionDirExisted = await extractionDir.exists();
+    if (!hasExtractionDirExisted) {
+      extractionDir.create();
     }
 
-    // Download the folder from OneDrive and save it to local storage
-    try {
-      final response = await dio.download(
-        'https://graph.microsoft.com/v1.0/me/drive/root:/Music',
-        '${downloadsDirectory}/Music.zip',
-        onReceiveProgress: (received, total) {
-          if (total != -1) {
-            print((received / total * 100).toStringAsFixed(0) + '%');
-          }
-        },
-      );
-      print(response.statusCode);
-      final taskId = await FlutterDownloader.enqueue(
-        url: 'https://graph.microsoft.com/v1.0/me/drive/root:/Music',
-        savedDir: downloadsDirectory,
-        fileName: 'Music.zip',
-        showNotification: true,
-        openFileFromNotification: true,
-      );
-      print('Download task ID: $taskId');
-    } catch (e) {
-      print(e);
+    // Download the zip file from the source URL
+    final zipUrl =
+        'http://drive.google.com/uc?id=1wGDrYfurNHnHyjIzsCaUy2oif3GJKlmB';
+    final zipPath = '$downloadsDirectory/music.zip';
+    final client = Dio();
+    await client.download(zipUrl, zipPath);
+
+    // Extract the contents of the zip file to the extraction directory
+    final bytes = File(zipPath).readAsBytesSync();
+    final archive = ZipDecoder().decodeBytes(bytes);
+    for (final file in archive) {
+      final filePath = '${extractionDirectory}/${file.name}';
+      if (file.isFile) {
+        final data = file.content as List<int>;
+        final extractedFile = File(filePath);
+        await extractedFile.create(recursive: true);
+        await extractedFile.writeAsBytes(data);
+      } else {
+        await Directory(filePath).create(recursive: true);
+      }
     }
+
+    // Delete the zip file after successful extraction
+    File(zipPath).deleteSync();
+
+    print('Extraction complete');
   }
 }
--- lib/view/home_screen/screen_drawer.dart
+++ lib/view/home_screen/screen_drawer.dart
@@ -13,7 +13,7 @@ import 'about_home.dart';
 class DrawerContent extends StatelessWidget {
   DrawerContent({super.key});
   final HomeScreenController _homeScreenController =
-      Get.put(HomeScreenController());
+  Get.put(HomeScreenController());
 
   @override
   Widget build(BuildContext context) {
@@ -21,66 +21,63 @@ class DrawerContent extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(26.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  functionText(appName, kWhiteColor, FontWeight.bold, 30),
-                  SizedBox(height: 10,),
-                  SizedBox(
-                    height: height * 0.01,
-                  ),
-                  functionText(
-                      '- A Bhajan Player', kWhiteColor, FontWeight.bold, 20),
-                  SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 40.0, bottom: 8.0, left:1, right: 10),
-                      child: Divider(
-                        color: Colors.grey,
-                        height: 2,
-                        thickness: 1,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    functionText(appName, kWhiteColor, FontWeight.bold, 30),
+                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: height * 0.01,
+                    ),
+                    functionText(
+                        '- A Bhajan Player', kWhiteColor, FontWeight.bold, 20),
+                    SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 40.0, bottom: 8.0, left:1, right: 10),
+                        child: Divider(
+                          color: Colors.grey,
+                          height: 2,
+                          thickness: 1,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 50,),
-                  Center(
-                      child: Image.asset(
-                        'assets/images/appIcon.png',
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      )
-                  ),
-                  SizedBox(height: 300,),
-                  SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 30.0, bottom: 8.0, left: 30, right: 10),
-                      child: TextButton(
-                        style: TextButton.styleFrom(backgroundColor: kAppbarColor, foregroundColor: Colors.white),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => AboutHome()),
-                          );
-                        },
-                        child: functionText('About Us', kWhiteColor, FontWeight.bold, 25),
+                    SizedBox(height: 75,),
+                    Center(
+                        child: Image.asset(
+                          'assets/images/appIcon.png',
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        )
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10.0, bottom: 8.0, left: 30, right: 10),
+                        child: TextButton(
+                          style: TextButton.styleFrom(backgroundColor: kAppbarColor, foregroundColor: Colors.white),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => AboutHome()),
+                            );
+                          },
+                          child: functionText('About Us', kWhiteColor, FontWeight.bold, 25),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
 
-                ]
+                  ]
               ),
+            ),
 
-            ],
-          ),
-
+          ],
         ),
+
       ),
     );
 
