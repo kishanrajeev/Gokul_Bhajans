df7f36b56df126abb0f42232ad294836a8f3a791
Kishan <kishanrj@outlook.com>
2023-02-25 17:19:14 -0500

    Made some changes to downlaod.dart. Still does not work.

--- lib/view/home_screen/downlaod.dart
+++ lib/view/home_screen/downlaod.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:dio/dio.dart';
 import 'package:archive/archive.dart';
 
 class Download extends StatefulWidget {
@@ -12,6 +10,8 @@ class Download extends StatefulWidget {
 }
 
 class _DownloadState extends State<Download> {
+  String _filePath = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,50 +19,64 @@ class _DownloadState extends State<Download> {
         title: Text('Download'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Download and Extract'),
-          onPressed: () => downloadAndExtract(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: Text('Download File'),
+              onPressed: () {
+                downloadFile().then((filePath) {
+                  setState(() {
+                    _filePath = filePath;
+                  });
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            if (_filePath.isNotEmpty) Text('File saved to: $_filePath'),
+          ],
         ),
       ),
     );
   }
 
-  void downloadAndExtract() async {
-    // Set up the download and extraction directories
-    final downloadsDirectory = (await getExternalStorageDirectory())!.path;
-    final extractionDirectory =
-        '${(await getApplicationDocumentsDirectory()).path}/Gokulbhajans';
-    final extractionDir = Directory(extractionDirectory);
-    bool hasExtractionDirExisted = await extractionDir.exists();
-    if (!hasExtractionDirExisted) {
-      extractionDir.create();
-    }
+  Future<String> downloadFile() async {
+    String url = 'http://drive.google.com/uc?id=1wGDrYfurNHnHyjIzsCaUy2oif3GJKlmB'; // Replace with your URL
+    String fileName = 'music.zip'; // Replace with your file name
+    String dir = (await getExternalStorageDirectory())!.path;
+    String filePath = '';
 
-    // Download the zip file from the source URL
-    final zipUrl =
-        'http://drive.google.com/uc?id=1wGDrYfurNHnHyjIzsCaUy2oif3GJKlmB';
-    final zipPath = '$downloadsDirectory/music.zip';
-    final client = Dio();
-    await client.download(zipUrl, zipPath);
+    HttpClient httpClient = HttpClient();
 
-    // Extract the contents of the zip file to the extraction directory
-    final bytes = File(zipPath).readAsBytesSync();
-    final archive = ZipDecoder().decodeBytes(bytes);
-    for (final file in archive) {
-      final filePath = '${extractionDirectory}/${file.name}';
-      if (file.isFile) {
-        final data = file.content as List<int>;
-        final extractedFile = File(filePath);
-        await extractedFile.create(recursive: true);
-        await extractedFile.writeAsBytes(data);
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+
+      if (response.statusCode == 200) {
+        var bytes = await consolidateHttpClientResponseBytes(response);
+        filePath = '$dir/$fileName';
+        File file = File(filePath);
+        await file.writeAsBytes(bytes);
+
+        // Extract the file
+        var archive = ZipDecoder().decodeBytes(bytes);
+        for (var file in archive) {
+          var filename = '$dir/${file.name}';
+          if (file.isFile) {
+            var outFile = File(filename);
+            outFile = await outFile.create(recursive: true);
+            await outFile.writeAsBytes(file.content as List<int>);
+          } else {
+            await Directory(filename).create(recursive: true);
+          }
+        }
       } else {
-        await Directory(filePath).create(recursive: true);
+        filePath = 'Error code: ' + response.statusCode.toString();
       }
+    } catch (ex) {
+      filePath = 'Can not fetch url';
     }
 
-    // Delete the zip file after successful extraction
-    File(zipPath).deleteSync();
-
-    print('Extraction complete');
+    return filePath;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import assets_audio_player
 import assets_audio_player_web
+import flutter_archive
 import path_provider_macos
 import share_plus_macos
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AssetsAudioPlayerPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerPlugin"))
   AssetsAudioPlayerWebPlugin.register(with: registry.registrar(forPlugin: "AssetsAudioPlayerWebPlugin"))
+  FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
