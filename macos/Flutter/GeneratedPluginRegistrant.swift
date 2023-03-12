5ddc4e3a70686ec344c7df730ec922d3865e63a7
Kishan <kishanrj@outlook.com>
2023-03-11 23:55:00 -0500

    Downloading now works. Make it so that the download page is now shown on first launch. When no bhajans are downloaded, it not only shows the option to download the bhajans.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,9 @@ import Foundation
 import assets_audio_player
 import assets_audio_player_web
 import flutter_archive
-import path_provider_macos
+import path_provider_foundation
 import share_plus_macos
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
